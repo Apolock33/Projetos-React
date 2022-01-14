a87068cb4553916fcb4b72aaaaaadddf7835ecd0
Apolock33 <carlosammgomes@gmail.com>
2022-01-13 22:49:55 -0300

    Projetos React

---
+++ Cronometro/cronometro/src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class App extends Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = {
+      numero: 0,
+      botao: 'COMEÇAR'
+    };
+    this.timer = null;
+    this.comecar = this.comecar.bind(this);
+    this.limpar = this.limpar.bind(this);
+  }
+
+  comecar() {
+    let state = this.state;
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+      this.state.botao = 'COMEÇAR'
+    } else {
+      this.timer = setInterval(() => {
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+      }, 100);
+      state.botao = 'PARAR';
+    }
+    this.setState(state);
+  }
+
+  limpar() {
+    if (this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    let state = this.state;
+    state.numero = 0;
+    state.botao = 'COMEÇAR'
+    this.setState(state);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <img className="img" src={require('./assets/cronometro.png')} />
+        <a className="timer">{this.state.numero.toFixed(1 )}</a>
+        <div className="areaBtn">
+          <a className="botao" onClick={this.comecar}>{this.state.botao}</a>
+          <a className="botao" onClick={this.limpar}>LIMPAR</a>
+        </div>  
+      </div>
+    );
+  }
+}
+
+export default App;
